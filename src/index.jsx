4ac376c01e42f6bda9e837433b5d042e8e5c0762
Andrew Ensley <aensley@users.noreply.github.com>
2026-05-20 15:06:07 -0500

    feat: start using scss (#32)
    Fixes #31

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootswatch/dist/materia/bootstrap.min.css'
-import './style.css'
+import './style.scss'
 import App from './App'
 
 createRoot(document.getElementById('app')).render(<App />)
